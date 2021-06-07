8cb2a2b07442f93dea7141d5f3860a639e6def6d
dariodiaz <dario.dad@gmail.com>
2021-06-07 11:33:26 -0300

    Adding tsconfig file

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,4 +1,4 @@
-/// <reference types="Cypress" />
+// /// <reference types="Cypress" />
 describe('Homepage tests', () => {
     it('Should load homepage and verify title', () => {
         cy.visit(Cypress.env('host')).title().should('eq', 'Hi! I\'m Dario Diaz - QA Engineer')
