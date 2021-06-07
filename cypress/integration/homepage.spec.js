c74e4b709e85de9981c2d306ee08a593d6fbb9a9
dariodiaz <dario.dad@gmail.com>
2021-06-07 16:04:22 -0300

    ARefactoring homepage tests to use PO

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,28 +1,38 @@
-// /// <reference types="Cypress" />
+import Navbar from "../support/components/Navbar";
+
 describe('Homepage tests', () => {
+    const navbar = new Navbar()
     beforeEach(() => {
         cy.visit(Cypress.env('host'))
         cy.viewport('macbook-15')
-    });
+    })
+
     it('Should load homepage and verify title', () => {
-        cy.title().should('eq', 'Hi! I\'m Dario Diaz - QA Engineer')
+        cy.title()
+            .should('eq', 'Hi! I\'m Dario Diaz - QA Engineer')
     })
 
     it('Should validate about me link', () => {
-        cy.get('#menu-item-26 > a').click()
-        cy.location('pathname').should('eq', '/about-me/')
-        cy.get('.post-title').should('have.text', 'About Me ')
+        navbar.visitAboutMe()
+        cy.location('pathname')
+            .should('eq', '/about-me/')
+        cy.get('.post-title')
+            .should('have.text', 'About Me ')
     })
 
     it('Should validate employment history link', () => {
-        cy.get('#menu-item-32 > a').click()
-        cy.location('pathname').should('eq', '/employment-history/')
-        cy.get('.post-title').should('have.text', 'Employment History ')
+        navbar.visitEmploymentHistory()
+        cy.location('pathname')
+            .should('eq', '/employment-history/')
+        cy.get('.post-title')
+            .should('have.text', 'Employment History ')
     })
 
     it('Should validate contact me link', () => {
-        cy.get('#menu-item-33 > a').click()
-        cy.location('pathname').should('eq', '/contact-me/')
-        cy.get('.post-title').should('have.text', 'Contact Me ')
+        navbar.visitContactMe()
+        cy.location('pathname')
+            .should('eq', '/contact-me/')
+        cy.get('.post-title')
+            .should('have.text', 'Contact Me ')
     })
 })
--- cypress/support/components/Navbar.js
+++ cypress/support/components/Navbar.js
@@ -1,15 +1,16 @@
 class Navbar {
     visitAboutMe() {
-
+        cy.get('#menu-item-26 > a').click()
     }
 
     visitEmploymentHistory() {
-
+        cy.get('#menu-item-32 > a').click()
     }
 
     visitContactMe() {
-        
+        cy.get('#menu-item-33 > a').click() 
     }
 }
-
-export default new Navbar()
+// TODO: 1. create Homepage and link it to this component
+// TODO: 2. Refactor the selectors, they are too brittle
+export default Navbar
